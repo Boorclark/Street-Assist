082087064b18242043865040670c48d11b19139b
Boorclark <clarkg@berea.edu>
2023-02-25 17:09:51 -0500

    Can now run with different cities and states

--- static/home.js
+++ static/home.js
@@ -4,4 +4,21 @@ $(document).ready(function(){
         $('#stateDropdownButton').text($(this).text());
     });
 
+    $("#submitButton").click(function(event) {
+        event.preventDefault();
+        var state = $("#stateDropdown .dropdown-item.active").data("value");
+        var city = $("#cityInput").val();
+        $.ajax({
+          url: "main.go",
+          type: "POST",
+          data: {state: state, city: city},
+          success: function(response) {
+            // handle successful response
+          },
+          error: function(xhr, status, error) {
+            // handle error response
+          }
+        });
+      });
+ 
 });
